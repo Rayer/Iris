f758cab87e7a802f80d4ef2efd167535a583abfc
Killercat <rayershih@gmail.com>
2017-07-27 21:38:41 +0800

    Initialize

---
+++ src/InventoryManager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Rayer Tung on 14/07/2017.
+//
+
+#include "InventoryManager.h"
---
+++ src/InventoryManager.h
@@ -0,0 +1,19 @@
+//
+// Created by Rayer Tung on 14/07/2017.
+//
+
+#ifndef IRIS_INVENTORYMANAGER_H
+#define IRIS_INVENTORYMANAGER_H
+
+#include <list>
+
+class Character;
+
+
+class InventoryManager {
+public:
+
+};
+
+
+#endif //IRIS_INVENTORYMANAGER_H
