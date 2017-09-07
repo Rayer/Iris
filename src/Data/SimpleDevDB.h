01946c649708cf332b4092372347d90732bcfbcb
Rayer <rayer@Rayers-MacBook.local>
2017-09-07 11:19:30 +0800

    Use filesystem::temp_directory_path to take place for /tmp for windows compabilities

--- src/Data/SimpleDevDB.h
+++ src/Data/SimpleDevDB.h
@@ -8,6 +8,7 @@
 
 #include "DataPersistenceLayer.h"
 #include "SimpleDevDBSpace.h"
+#include <boost/filesystem.hpp>
 
 /**
  * DPL Implementation for development
@@ -17,8 +18,8 @@ protected:
     std::map<std::string, std::shared_ptr<SimpleDevDBSpace>> space_map;
 
 public:
-    void serialize(std::string folder = "/tmp");
-    void deserialize(std::string folder = "/tmp");
+    void serialize(std::string folder = boost::filesystem::complete(boost::filesystem::temp_directory_path()).generic_string());
+    void deserialize(std::string folder = boost::filesystem::complete(boost::filesystem::temp_directory_path()).generic_string());
     std::shared_ptr<Space> get_space(const std::string &&name) override;
     std::shared_ptr<Space> get_space(long id) override;
 
